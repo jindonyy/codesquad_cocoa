27071f9ab9ae2773d992f60093962e2838e70cf9
Jindonyy <g01056767919@gmail.com>
2021-11-03 23:38:45 +0900

    숫자타입의 배열 만들기

---
+++ day_02/mission2/answer_4.js
@@ -0,0 +1,45 @@
+const data = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": { 
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+};
+
+function getNumElement(data) {
+    let numValue = [];
+
+    function differentiateType(obj) {    
+        for(let key in obj) {
+            const el = obj[key];
+            if(el instanceof Object) differentiateType(el);
+            else {
+                if(typeof(el) === 'number') numValue.push(key);
+            }
+        }    
+    }
+    differentiateType(data);
+
+    return numValue;
+}
+
+console.log(getNumElement(data));
