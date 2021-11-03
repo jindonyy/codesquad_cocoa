c51c9ece33c4034ca8a1ea3ed0bf5bea25848d60
Jindonyy <g01056767919@gmail.com>
2021-11-04 01:01:14 +0900

    잘못된 문자 요소 거르기

---
+++ day_02/mission2/answer_2.js
@@ -0,0 +1,44 @@
+const people = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+const regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-+<>@\#$%&\\\=\(\'\"]/gi;
+
+// for문 사용
+function removeNum(person) {
+    for(let i = 0, len = person.length; i < len; i++) {
+        const el = person[i];
+        if(!isNaN(Number(el))) person = person.replace(el, "");
+    }
+    return person;
+}
+
+function getNewPerson(people) {
+    let newPeople = [];
+
+    for(let i = 0, len = people.length; i < len; i++) {
+        const person = people[i];
+        if(!regExp.test(person)) newPeople.push(removeNum(person));
+    }
+    return newPeople;
+}
+
+console.log(getNewPerson(people));
+
+
+
+// forEach, filter 사용
+function removeNum2(person) {
+    let split = person.split("");
+
+    split = split.filter(el => isNaN(Number(el)));
+    return split.join("");
+}
+
+function getNewPerson2(people) {
+    let newPeople = [];
+
+    people.forEach(person => {
+        if(!regExp.test(person)) newPeople.push(removeNum2(person));
+    });
+    return newPeople;
+}
+
+console.log(getNewPerson2(people));
