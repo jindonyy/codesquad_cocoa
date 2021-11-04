c2e3fc3459715dcd7feed3a87da90129e6eea0c8
Jindonyy <g01056767919@gmail.com>
2021-11-04 19:06:36 +0900

    5. 고차원 객체 탐색

---
+++ day_02/mission2/answer_5.js
@@ -0,0 +1,106 @@
+const data = [{
+	"id": 1,
+	"name": "Yong",
+	"phone": "010-0000-0000",
+	"type": "sk",
+	"childnode": [{
+		"id": 11,
+		"name": "echo",
+		"phone": "010-0000-1111",
+		"type": "kt",
+		"childnode": [{
+				"id": 115,
+				"name": "hary",
+				"phone": "211-1111-0000",
+				"type": "sk",
+				"childnode": [{
+					"id": 1159,
+					"name": "pobi",
+					"phone": "010-444-000",
+					"type": "kt",
+					"childnode": [{
+							"id": 11592,
+							"name": "cherry",
+							"phone": "111-222-0000",
+							"type": "lg",
+							"childnode": []
+						},
+						{
+							"id": 11595,
+							"name": "solvin",
+							"phone": "010-000-3333",
+							"type": "sk",
+							"childnode": []
+						}
+					]
+				}]
+			},
+			{
+				"id": 116,
+				"name": "kim",
+				"phone": "444-111-0200",
+				"type": "kt",
+				"childnode": [{
+					"id": 1168,
+					"name": "hani",
+					"phone": "010-222-0000",
+					"type": "sk",
+					"childnode": [{
+						"id": 11689,
+						"name": "ho",
+						"phone": "010-000-0000",
+						"type": "kt",
+						"childnode": [{
+								"id": 116890,
+								"name": "wonsuk",
+								"phone": "010-000-0000",
+								"type": "kt",
+								"childnode": []
+							},
+							{
+								"id": 1168901,
+								"name": "chulsu",
+								"phone": "010-0000-0000",
+								"type": "sk",
+								"childnode": []
+							}
+						]
+					}]
+				}]
+			},
+			{
+				"id": 117,
+				"name": "hong",
+				"phone": "010-0000-0000",
+				"type": "lg",
+				"childnode": []
+			}
+		]
+	}]
+}];
+
+function getSkName(data) {
+    let skNameArr = [];
+
+	function searchSk(obj) {
+		if(obj.type === "sk") skNameArr.push(obj.name);
+		for(let key in obj) {
+            const el = obj[key];
+			if(Array.isArray(el)) loopArray(el);
+		}
+	}
+
+    function loopArray(arr) {    
+        arr.forEach(el => {
+			// if(Object.prototype.toString.call(el) === '[object Object]') searchSk(el);
+			searchSk(el);
+		}); 
+    }
+    loopArray(data);
+
+    return skNameArr;
+}
+
+
+
+console.log(getSkName(data));
