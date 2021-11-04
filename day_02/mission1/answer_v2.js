70ba472090577e2c528d5da8d4f9e410c93979e3
Jindonyy <g01056767919@gmail.com>
2021-11-04 14:51:44 +0900

    파일명 변경

--- day_02/mission1/answer_v2.js
+++ day_02/mission1/answer_v2.js
@@ -1,23 +1,28 @@
-function changeNnum(n, t, num) {
-    let numArr = [];
-
-    for(let i = num, len = num + t; i < len; i++) {
-        numArr.push(...i.toString(n).split(""));
-    }
-    return numArr;
+function changeNnum(n, num) {
+    return num.toString(n).split("");
 }
 
 function getResultArr(n, t, m) {
     let resultArr = [];
 
-    for(let i = 0, len = t * m; i < len; i+=t) {
-        resultArr.push(...changeNnum(n, t, i));
+    for(let i = 0, len = t * m; i < len; i++) {
+        resultArr.push(...changeNnum(n, i));
     }
     return resultArr;
 }
 
 function getPNum(n, t, m, p) {
-    return changeNnum(n, t, t * (p-1));
+    const resultArr = getResultArr(n, t, m);
+    let start = 0;
+    let end = 0;
+    
+    for(let i = 0, len = t * m; i < len; i++) {
+        const numLen = changeNnum(n, i).length;
+        if(i > t * p) break;
+        if(i >= t * (p-1)) end += numLen;
+        else start += numLen;
+    }
+    return resultArr.slice(start, start + end);
 }
 
-console.log(getPNum(2, 4, 3, 2)); // ['1', '0', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1']
+console.log(getPNum(2, 4, 2, 2)); // ['1', '0', '0', '1', '0', '1', '1', '1', '0', '1', '1', '1']
