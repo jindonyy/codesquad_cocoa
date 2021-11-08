b263b47a03508338406c5561e16404da74aec456
Jindonyy <g01056767919@gmail.com>
2021-11-09 04:52:49 +0900

    배열 사용해서 다시 풀기

--- day_04/answer.js
+++ day_04/answer.js
@@ -1,43 +1,63 @@
-function hashMap() {}
+const hashFun = (key) => key.length % 10;
+const hashMap = function() {
+    this.hashTable = [];
+}
 hashMap.prototype.put = function(key, value) {
-    hash.containsKey(key) ? this[key] = value : hash.replace(key, value);
+    const hash = hashFun(key);
+    this.hashTable[hash] ? this.hashTable[hash][key] = value : this.hashTable[hash] = {[key]: value};
 }
 hashMap.prototype.remove = function(key) {
-    delete this[key];
+    const hash = hashFun(key);
+    const cur = this.hashTable[hash];
+    if(cur) delete cur[key];
 }
 hashMap.prototype.containsKey = function(key) {
-    return this[key] !== undefined ? true : false;
+    const hash = hashFun(key);
+    const cur = this.hashTable[hash];
+    return cur[key] !== undefined ? true : false;
 }
 hashMap.prototype.get = function(key) {
-    return this[key];
+    const hash = hashFun(key);
+    const cur = this.hashTable[hash];
+    return cur[key];
 }
 hashMap.prototype.isEmpty = function() {
-    return hash.keys().length === 0 ? true : false;
+    return this.hashTable.length ? false : true;
 }
 hashMap.prototype.keys = function() {
-    return Object.keys(this);
+    const keysArr = [];
+    this.hashTable.forEach(el => {
+        if(el) keysArr.push(...Object.keys(el));
+    });
+    return keysArr;
 }
 hashMap.prototype.replace = function(key, value) {
-    
+    this.hashTable.forEach(el => {
+        if(el && el[key] !== undefined) el[key] = value; 
+    });
 }
 hashMap.prototype.size = function() {
-    return hash.keys().length;
+    let size = 0;
+    this.hashTable.forEach(el => {
+        if(el) {
+            size += Array.isArray(el) ? el.length : 1;
+        }
+    })
+    return size;
 }
 hashMap.prototype.clear = function() {
-    for (var key in hash) {
-        delete hash[key];
-    }
+    this.hashTable.splice(0, this.hashTable.length);
 }
 
-const hash = new hashMap();
+const map = new hashMap();
 
 
-hash.put('a', 1);
-hash.put('A', 1);
-hash.remove('A');
-console.log(hash.containsKey('A'));
-console.log(hash.get('a'));
-console.log(hash.isEmpty());
-console.log(hash.keys());
-console.log(hash.size());
-hash.clear();
+map.put('a', 1); // hashTable: [ <1 empty item>, { a: 1 } ]
+map.put('b', 1.5); // hashTable: [ <1 empty item>, { a: 1 , b: 1.5} ]
+map.remove('b'); // hashTable: [ <1 empty item>, { a: 1 } ]
+console.log(map.containsKey('b')); // false
+console.log(map.get('a')); // 1
+console.log(map.isEmpty()); // false
+console.log(map.keys()); // ['a']
+console.log(map.size()); // 1
+map.clear(); //hashTable: []
---
+++ day_04/answer_v2.js
@@ -0,0 +1,43 @@
+function hashMap() {}
+hashMap.prototype.put = function(key, value) {
+    this[key] = value;
+}
+hashMap.prototype.remove = function(key) {
+    delete this[key];
+}
+hashMap.prototype.containsKey = function(key) {
+    return this[key] !== undefined ? true : false;
+}
+hashMap.prototype.get = function(key) {
+    return this[key];
+}
+hashMap.prototype.isEmpty = function() {
+    return hash.keys().length === 0 ? true : false;
+}
+hashMap.prototype.keys = function() {
+    return Object.keys(this);
+}
+hashMap.prototype.replace = function(key, value) {
+    this[key] = value;
+}
+hashMap.prototype.size = function() {
+    return hash.keys().length;
+}
+hashMap.prototype.clear = function() {
+    for (var key in hash) {
+        delete hash[key];
+    }
+}
+
+const hash = new hashMap();
+
+
+hash.put('a', 1);
+hash.put('A', 1);
+hash.remove('A');
+console.log(hash.containsKey('A'));
+console.log(hash.get('a'));
+console.log(hash.isEmpty());
+console.log(hash.keys());
+console.log(hash.size());
+hash.clear();
