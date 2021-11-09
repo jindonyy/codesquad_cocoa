2cec936394e489bbc335e95e1069012b02c604bc
Jindonyy <g01056767919@gmail.com>
2021-11-09 23:30:37 +0900

    _object파일을 class로 변경

--- day_04/answer_class.js
+++ day_04/answer_class.js
@@ -1,68 +1,47 @@
-const hashFun = (key) => key.length % 10;
 class hashMap {
-    constructor() {
-        this.hashTable = [];
-    }
+    constructor() { }
     put(key, value) {
-        const hash = hashFun(key);
-        this.hashTable[hash] ? this.hashTable[hash][key] = value : this.hashTable[hash] = { [key]: value };
+        this[key] = value;
     }
     remove(key) {
-        const hash = hashFun(key);
-        const cur = this.hashTable[hash];
-        if (cur)
-            delete cur[key];
+        delete this[key];
     }
     containsKey(key) {
-        const hash = hashFun(key);
-        const cur = this.hashTable[hash];
-        return cur[key] !== undefined ? true : false;
+        return this[key] !== undefined ? true : false;
     }
     get(key) {
-        const hash = hashFun(key);
-        const cur = this.hashTable[hash];
-        return cur[key];
+        return this[key];
     }
     isEmpty() {
-        return this.hashTable.length ? false : true;
+        return hashMap.prototypekeys().length === 0 ? true : false;
     }
     keys() {
-        const keysArr = [];
-        this.hashTable.forEach(el => {
-            if (el)
-                keysArr.push(...Object.keys(el));
-        });
-        return keysArr;
+        return Object.keys(this);
     }
     replace(key, value) {
-        this.hashTable.forEach(el => {
-            if (el && el[key] !== undefined)
-                el[key] = value;
-        });
+        this[key] = value;
     }
     size() {
-        let size = 0;
-        this.hashTable.forEach(el => {
-            if (el) {
-                size += Array.isArray(el) ? el.length : 1;
-            }
-        });
-        return size;
+        return hashMap.prototypekeys().length;
     }
     clear() {
-        this.hashTable.splice(0, this.hashTable.length);
+        for (var key in map) {
+            delete map[key];
+        }
     }
 }
 
 const map = new hashMap();
 
 
-map.put('a', 1); // hashTable: [ <1 empty item>, { a: 1 } ]
-map.put('b', 1.5); // hashTable: [ <1 empty item>, { a: 1 , b: 1.5} ]
-map.remove('b'); // hashTable: [ <1 empty item>, { a: 1 } ]
-console.log(map.containsKey('b')); // false
+map.put('a', 1);
+map.put('b', 2);
+map.put('c', 3); // hashMap { a: 1, b: 2, c: 3 }
+map.remove('b'); // hashMap { a: 1, c: 3 }
+console.log(map.containsKey('A')); // false
 console.log(map.get('a')); // 1
 console.log(map.isEmpty()); // false
-console.log(map.keys()); // ['a']
-console.log(map.size()); // 1
-map.clear(); //hashTable: []
+console.log(map.keys()); // ['a', 'c']
+map.replace('a', 0); // hashMap { a: 0, c: 3 }
+console.log(map.size()); // 2
+map.clear(); // hashMap {}
