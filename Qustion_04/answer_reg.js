a37b6addd894f412d365f2d05ca10d38b1800bff
Jindonyy <g01056767919@gmail.com>
2021-11-11 13:36:32 +0900

    1~2문제 if문 수정

--- Qustion_04/answer_reg.js
+++ Qustion_04/answer_reg.js
@@ -6,12 +6,11 @@ function analyzeArr() {
 }
 
 // 1~2. 배열정보 출력 및 올바른 배열인지 체크
-analyzeArr.prototype.checkTrueArr = function(data) {
+analyzeArr.prototype.getDataInfo = function(data) {
     const openBracketArr = data.match(/\[/g);
     const closeBracketArr = data.match(/\]/g);
     
-    if(openBracketArr.length === closeBracketArr.length) return true;
-    else if(openBracketArr.length > closeBracketArr.length) {
+    if(openBracketArr.length > closeBracketArr.length) {
         console.log('닫는 괄호가 일치하지 않습니다.');
         return false;
     } else if(openBracketArr.length < closeBracketArr.length) {
@@ -25,22 +24,23 @@ analyzeArr.prototype.checkTrueArr = function(data) {
 
 // 3. 배열 구조 출력
 analyzeArr.prototype.getDataStructure = function(data) {
-    if(!arrData.checkTrueArr(data)) return false;
-
-    const splitData = data.split(",");
-    for(let i = 0, len = data.length; i < len; i++) {
-        const el = data[i];
-        if(el === ' ' || el === ',') continue;
-        if(el === '[') this.dataInfo.braket++;
-        else if(el === ',') this.dataInfo.number++;
+    try {
+        data = JSON.parse(data);
+    } catch(e){
+        console.log('열고 닫는 괄호를 확인해주세요.');
+        return false;
     }
 
-    console.log(this.dataStructure);
+    const dataStructure = {};
+
+
+
+    console.log(dataStructure);
 }
 
 const arrData = new analyzeArr();
 
 
-arrData.checkTrueArr(wrongData2); // 여는 괄호가 일치하지 않습니다.
-arrData.printDataInfo(data); // 배열의 중첩된 깊이 수준은 4이며, 총 6개의 원소가 포함되어 있습니다.
+arrData.getDataInfo(wrongData2); // 여는 괄호가 일치하지 않습니다.
+arrData.getDataInfo(data); // 여는 괄호가 일치하지 않습니다.
 arrData.getDataStructure(data); // 
