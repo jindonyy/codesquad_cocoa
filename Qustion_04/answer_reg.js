f8d5eb1d3d19a9e314893983286ee5d7f408274d
Jindonyy <g01056767919@gmail.com>
2021-11-11 13:03:09 +0900

    1~2 문제 수정

--- Qustion_04/answer_reg.js
+++ Qustion_04/answer_reg.js
@@ -3,19 +3,13 @@ const wrongData1 = "[111,2,[33,4,[5,[6]]]";
 const wrongData2 = "[111,2,[33,4,[5,[6]]]]]]";
 
 function analyzeArr() {
-    this.bracketArr = [];
-    this.dataInfo = {
-        braket: 0,
-        number: 0
-    }
-    this.dataStructure = {};
 }
 
-// 2. 올바른 배열인지 체크
+// 1~2. 배열정보 출력 및 올바른 배열인지 체크
 analyzeArr.prototype.checkTrueArr = function(data) {
     const openBracketArr = data.match(/\[/g);
-    const closeBracketArr = data.match(/\]/gi);
-
+    const closeBracketArr = data.match(/\]/g);
+    
     if(openBracketArr.length === closeBracketArr.length) return true;
     else if(openBracketArr.length > closeBracketArr.length) {
         console.log('닫는 괄호가 일치하지 않습니다.');
@@ -24,15 +18,8 @@ analyzeArr.prototype.checkTrueArr = function(data) {
         console.log('여는 괄호가 일치하지 않습니다.');
         return false;
     }
-}
-
-// 1. 배열 정보 출력
-analyzeArr.prototype.printDataInfo = function(data) {
-    if(!arrData.checkTrueArr(data)) return false;
-
-    const openBracketArr = data.match(/\[/gi);
-    const commaArr = data.match(/\,/gi);
-
+    
+    const commaArr = data.match(/\,/g);
     console.log(`배열의 중첩된 깊이 수준은 ${openBracketArr.length}이며, 총 ${commaArr.length+1}개의 원소가 포함되어 있습니다.`);
 }
 
