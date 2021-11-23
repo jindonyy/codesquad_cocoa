1c9ae91408c2ad7885782e51aa9f6fc10bf0a2ff
dony <apple93do3@naver.com>
2021-11-23 12:50:10 +0900

    innerHTML로 수정, list 추가 및 삭제 기능 구현

--- mission_07/resources/js/ui.js
+++ mission_07/resources/js/ui.js
@@ -2,115 +2,120 @@ class ToDoModel {
     constructor() {
         this.storageKey = 'to-do-list';
         this.$addInput = document.querySelector('.addFormWrap input');
+        this.toDoData = {
+            'txt': '',
+            'date': this.getDate(),
+            'complete': 'incomplete'
+        };
         this.toDoDataArr = [];
     }
     initData () {
         const storage = localStorage.getItem(this.storageKey);
         if(storage) return this.toDoDataArr = JSON.parse(storage);
     }
-    getTxt() {
-        const toDoTxt = this.$addInput.value;
-        return toDoTxt === "" ? false : toDoTxt;
-    }
     getDate() {
         const newDate = new Date(); 
         return `${newDate.getFullYear()}.${newDate.getMonth() + 1}.${newDate.getDate()}`;
     }
-    // getSuccess() {
-        
-    // }
     saveData() {
-        if(!this.getTxt()) return false;
-        const toDoData = {
-            'txt': this.getTxt(),
-            'date': this.getDate(),
-            // 'success': this.getSuccess()
-        };
-        this.toDoDataArr.push(toDoData);
+        this.toDoDataArr.push(this.toDoData);
         localStorage.setItem(this.storageKey, JSON.stringify(this.toDoDataArr));
-        return toDoData;
     }
-    deleteData(e) {
-        e.preventDefault();
-        const $deleteBtn = e.currentTarget;
-        const dataTodelete = $deleteBtn.parentNode.querySelector('.txt').innerText
-        this.toDoDataArr = this.toDoDataArr.filter(el => el.txt !== dataTodelete);
+    deleteData(data) {
+        this.toDoDataArr = this.toDoDataArr.filter(el => el.txt !== data);
         localStorage.setItem(this.storageKey, JSON.stringify(this.toDoDataArr));
-        $deleteBtn.parentNode.remove();
     }
 }
 
 class ToDoView {
+    constructor() {
+        this.$addBtn = document.querySelector('.addBtn');
+        this.$addInput = document.querySelector('.addFormWrap input');
+    }
     initView(data) {
-        data.forEach((el) => this.view.addToDoList(el));
+        data.forEach((el) => this.renderToDo(el));
     }
-    checkSuccess(e) {
-        e.preventDefault();
-        const $chk = e.currentTarget;
-        $chk.parentNode.classList.toggle('success');
+    renderToDo(list) {
+        document.querySelector('.toDoList').appendChild(list);
+        this.$addInput.value = "";
     }
-    addChkBox() {
-        const $chk = document.createElement('input');
-        $chk.type = 'checkbox';
-        $chk.addEventListener('click', (e) => this.checkSuccess(e));
-        return $chk;
-    }
-    addTxt(txt) {
-        const $txt = document.createElement('span');
-        $txt.classList.add('txt');
-        $txt.innerText = txt;
-        document.querySelector('.addFormWrap input').value = "";
-        return $txt;
-    }
-    addDate(date) {
-        const $date = document.createElement('span');
-        $date.classList.add('date');
-        $date.innerText = date;
-        return $date;
-    }
-    addSuccessBtn() {
-        const $successBtn = document.createElement('button');
-        $successBtn.classList.add('successBtn');
-        return $successBtn;
-    }
-    addDelBtn() {
-        const $deleteBtn = document.createElement('button');
-        $deleteBtn.classList.add('deleteBtn');
-        return $deleteBtn;
-    }
-    addToDoList(data) {
-        const $li = document.createElement('li');
-        $li.append(this.addChkBox(), this.addTxt(data.txt), this.addDate(data.date), this.addSuccessBtn(), this.addDelBtn());
-        document.querySelector('.toDoList').appendChild($li);
+    deleteToDo(list) {
+        list.parentNode.parentNode.remove();
     }
 }
 
 class ToDoController {
-    constructor(data, view) {
-        this.data = data;
+    constructor(model, view) {
+        this.model = model;
         this.view = view;
-        this.$addBtn = document.querySelector('.addBtn');
-        this.$addInput = document.querySelector('.addFormWrap input');
     }
     initToDo() {
-        const data = this.data.initData();
-        this.view.initView(data); 
+        const storageData = this.model.initData();
+        if(storageData) storageData.forEach((el) => this.view.renderToDo(this.addToDo(el))); 
+    }
+    addBtnEvent() {
+        const toDoTxt = this.model.$addInput.value;
+        if(!toDoTxt) return false;
+        this.model.toDoData.txt = toDoTxt;
+        this.model.saveData();
+        this.view.renderToDo(this.addToDo(this.model.toDoData));
     }
     initEvent() {
-        this.$addBtn.addEventListener('click', function() {
-            const data = this.data.saveData();
-            this.view.addToDoList(data);
+        this.view.$addBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.addBtnEvent();
         });
-        this.$addInput.addEventListener('keyup', function(e) {
-            if(e.keyCode == 13) {
-                const data = this.data.saveData();
-                this.view.addToDoList(data);
-            }
+        this.view.$addInput.addEventListener('keyup', (e) => {
+            if(e.keyCode == 13) this.addBtnEvent();
         });
+    }
+
+    createCompleteBtn() {
+        const $completeBtn = document.createElement('button');
+        $completeBtn.classList.add('completeBtn');
+        $completeBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const target = e.currentTarget;
+            target.parentNode.parentNode.classList.toggle('complete');
+            this.model.toDoData.complete = (target.parentNode.parentNode.classList.contains('complete')) ? 'complete': 'incomplete';
+            this.model.saveData();
+        });
+        return $completeBtn;
+    }
+    createEditBtn() {
+        const $editBtn = document.createElement('button');
+        $editBtn.classList.add('editBtn');
+        // $editBtn.addEventListener('click', (e) => {
+        //     e.preventDefault();
+        //     const target = e.currentTarget;
+        //     target.parentNode.querySelector();
+        // });
+        return $editBtn;
+    }
+    createDeleteBtn() {
+        const $deleteBtn = document.createElement('button');
+        $deleteBtn.classList.add('deleteBtn');
+        $deleteBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+            const dataTodelete = $deleteBtn.parentNode.parentNode.querySelector('.txt').innerText;
+            this.model.deleteData(dataTodelete);
+            this.view.deleteToDo($deleteBtn);
+        });
+        return $deleteBtn;
+    }
+    addToDo(data) {
+        const $li = document.createElement('li');
+        $li.innerHTML = `<input type="checkbox">
+                        <span class="txt">${data.txt}</span>
+                        <span class="date">${data.date}</span>
+                        <div class="btnWrap">
+                        </div>`;
+        $li.querySelector('.btnWrap').append(this.createCompleteBtn(), this.createEditBtn(), this.createDeleteBtn());
+        return $li;
     }
 }
 
 
-const toDoList = new ToDoController(new ToDoModel, new ToDoView);
+const toDoList = new ToDoController(new ToDoModel(), new ToDoView());
 toDoList.initToDo();
 toDoList.initEvent();
