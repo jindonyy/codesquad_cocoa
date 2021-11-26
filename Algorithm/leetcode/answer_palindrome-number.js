9d9156f5bab9c9516eee18158fcacd3d87efbb6e
dony <apple93do3@naver.com>
2021-11-27 02:46:33 +0900

    String -> toStirng

--- Algorithm/leetcode/answer_palindrome-number.js
+++ Algorithm/leetcode/answer_palindrome-number.js
@@ -6,6 +6,6 @@ var isPalindrome = function(x) {
     if(x < 0) return false;
     if(x < 10) return true;
     
-    let str = String(x);
+    let str = x.toString();
     return str.split('').reverse().join('') === str ? true : false;
 };
